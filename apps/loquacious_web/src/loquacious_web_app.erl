5acd54de13d87da1996a3518a8370c538577d20c
Sebastian Green-Husted <ricecake@tfm.nu>
2016-07-11 18:45:51 -0400

    add basic routing

--- apps/loquacious_web/src/loquacious_web_app.erl
+++ apps/loquacious_web/src/loquacious_web_app.erl
@@ -15,12 +15,26 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    loquacious_web_sup:start_link().
+	case loquacious_web_sup:start_link() of
+		{ok, Pid} ->
+			Dispatch = cowboy_router:compile([
+				{'_', [
+					% Static File route
+					{"/static/[...]", cowboy_static, {priv_dir, loquacious_web, "static/"}},
+					% Dynamic Pages
+					{"/", loquacious_web_page, index}
+				]}
+			]),
+			{ok, _} = cowboy:start_http(loquacious_web, 25, [{ip, {127,0,0,1}}, {port, 8686}],
+							[{env, [{dispatch, Dispatch}]}]),
+			{ok, Pid}
+	end.
 
 %%--------------------------------------------------------------------
-stop(_State) ->
-    ok.
 
-%%====================================================================
+stop(_State) ->
+	ok.
+
+%%====================================================================1
 %% Internal functions
 %%====================================================================
